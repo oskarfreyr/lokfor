f8a8324e875bdfb0395ead124faead29a0f71670
oskarfreyr <oskarfreyr00@gmail.com>
2017-05-12 02:45:16 +0000

    Update lokaverkefni.py

--- lokaverkefni.py
+++ lokaverkefni.py
@@ -1,4 +1,4 @@
-﻿import random                                                                                                           #Skráin er opnuð
+import random                                                                                                           #Skráin er opnuð
 with open("kindur.txt") as kindur:
     frsl = kindur.read().split("  ")
 randomicer = random.sample(range(52), 52)                                                                               #Spilin eru stokkuð til að randomica þau og listarnir eru búnir til
@@ -6,13 +6,13 @@ player1spil = []
 cpuspil = []
 player1 = []
 cpu = []
-jafntefli = []
+Jafntefli = []
 for x in randomicer:
     if x % 2 == 0:
         cpuspil.append(x)
     else:
         player1spil.append(x)
-for x in player1:													#spilin eru sett í annaðhvort 
+for x in player1:													#spilin eru sett í annaðhvort
     player1.append(frsl[x])
 for x in cpu:
     cpu.append(frsl[x])
@@ -37,11 +37,11 @@ while len(cpu) > 0 and len(player1) > 0:
     flokkur2 = spil1[1].split(",")
     if (flokkur2[val - 1]) > (flokkur1[val - 1]):
         print("Cpu vann")
-        print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\")
-        if len(jafntefli) > 0:
-            for kindur in jafntefli:
-                cpu.append(kindur)
-            jafntefli.clear()
+        print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+        if len(Jafntefli) > 0:
+            for kindur1 in Jafntefli:
+                cpu.append(kindur1)
+            Jafntefli.clear()
         cpu.append(cpu[0])
         cpu.remove(cpu[0])
         cpu.append(player1[0])
@@ -49,11 +49,11 @@ while len(cpu) > 0 and len(player1) > 0:
         print(player1)
     if (flokkur1[val - 1])(flokkur2[val - 1]):
         print("Leikmaður vann")
-        print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\")
-        if len(jafntefli) > 0:
-            for kindur in jafntefli:
-                player1.append(kindur)
-            jafntefli.clear()
+        print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+        if len(Jafntefli) > 0:
+            for kindur1 in Jafntefli:
+                player1.append(kindur1)
+            Jafntefli.clear()
         player1.append(player1[0])
         player1.remove(player1[0])
         player1.append(cpu[0])
@@ -61,50 +61,50 @@ while len(cpu) > 0 and len(player1) > 0:
         print(cpu)
     else:
         print("Jafntefli")
-        jafntefli.append(player1[0])
-        jafntefli.append(cpu[0])
+        Jafntefli.append(player1[0])
+        Jafntefli.append(cpu[0])
         player1.remove(player1[0])
         cpu.remove(cpu[0])
-        print(jafntefli)
-        print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\")
+        print(Jafntefli)
+        print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
         print("cpu á að gera")
         val = random.randint(0, 7)
         flokkur1 = spil[1].split(",")
         flokkur2 = spil1[1].split(",")
-        jafntefli = []
+        Jafntefli = []
     if (flokkur1[val]) > (flokkur2[val]):
         print("Leikmaður vann")
-        print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\")
-        if len(jafntefli) > 0:
-            for kindur in jafntefli:
-                player1.append(kindur)
-            jafntefli.clear()
+        print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+        if len(Jafntefli) > 0:
+            for kindur1 in Jafntefli:
+                player1.append(kindur1)
+            Jafntefli.clear()
         player1.append(player1[0])
         player1.remove(player1[0])
         player1.append(cpu[0])
         cpu.remove(cpu[0])
     if (flokkur2[val]) > (flokkur1[val]):
         print("Cpu vann")
-	print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\")
-        if len(jafntefli) > 0:
-            for kindur in jafntefli:
-                cpu.append(kindur)
-            jafntefli.clear()
-        cpu.append(jafntefli)
+        print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+        if len(Jafntefli) > 0:
+            for kindur1 in Jafntefli:
+                cpu.append(kindur1)
+            Jafntefli.clear()
+        cpu.append(Jafntefli)
         cpu.append(cpu[0])
         cpu.remove(cpu[0])
         cpu.append(player1[0])
         player1.remove(player1[0])
         print(player1)
     else:                                                                                                               #hérna er skrifað hver sigurvegarinn er eða hvort það var jafntefli.
-        print("Jafntefli")
-        jafntefli.append(player1[0])
-        jafntefli.append(cpu[0])
-        print(jafntefli)
-        print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\")
+        print("jafntefli")
+        Jafntefli.append(player1[0])
+        Jafntefli.append(cpu[0])
+        print(Jafntefli)
+        print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
 if len(player1) == 0:
-    print("Cpu vann leikinn")
-    print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\")
+    print("Cpu vann")
+    print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
 elif len(cpu) == 0:
-    print("Leikmaður vann leikinn")
-    print("/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\")
+    print("Leikmaður vann")
+    print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
